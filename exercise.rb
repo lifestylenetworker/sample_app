b4f16fbfac417d9a397ae9b753345b5a6f541359
Nick Martin <nick@nickmartin.com>
2013-05-28 22:23:20 +0800

    Adding exercises

---
+++ exercise.rb
@@ -0,0 +1,11 @@
+class Shuffle
+
+	def initialize()
+	end
+
+	def shuffleWord(s)
+
+		s.split('').shuffle.join
+		
+	end
+end
---
+++ hashes.rb
@@ -0,0 +1,19 @@
+class Hashes
+
+	def initialize()
+
+		@person1 = {first: "Tom", last: "Hansen"}
+		@person2 = {first: "Lise", last: "Hansen"}
+		@person3 = {first: "Signe", last: "Hansen"}
+
+		@params = {father: @person1, mother: @person2, daughter: @person3}
+	end
+
+	def print()
+		@params
+	end
+
+	def print_father()
+		@params[:father][:first]
+	end
+end
